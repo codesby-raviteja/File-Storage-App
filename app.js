bf3921d712cdc2bf2fcea5bb13e496d87432f966
codesby-raviteja <raviteja555@gmail.com>
2025-07-03 15:40:40 +0530

    change cors Oring:

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://teja-drive-storage.netlify.app',
   credentials:true
 }))
 app.use("/", authRouter)
